bc5fd2f1e62237cb549857f06cf4860cea186711
jayson612 <jayson123@naver.com>
2025-03-09 21:22:51 +0900

    게시글 검색 기능 추가

--- src/post/dto/query/get-post-page-query.dto.ts
+++ src/post/dto/query/get-post-page-query.dto.ts
@@ -1,4 +1,36 @@
+import { Optional } from '@nestjs/common';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 import { PageQueryDto } from 'src/common/dto/query/page-query.dto';
 
-export class GetPostPageQueryDto extends PageQueryDto {}
+export class GetPostPageQueryDto extends PageQueryDto {
+  @ApiProperty({
+    description: '제목 검색어',
+    example: '제목 검색어',
+    type: String,
+    required: false,
+  })
+  @Optional()
+  @IsString()
+  title?: string;
+
+  @ApiProperty({
+    description: '내용 검색어',
+    example: '내용 검색어',
+    type: String,
+    required: false,
+  })
+  @Optional()
+  @IsString()
+  content?: string;
+
+  @ApiProperty({
+    description: '작성자 ID',
+    example: 'MiMiMinGyu',
+    type: String,
+    required: false,
+  })
+  @Optional()
+  @IsString()
+  userId?: string;
+}
